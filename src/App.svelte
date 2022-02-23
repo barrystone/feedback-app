d0fe864833aca4fe3c0834c1d65a457c3dd880d7
barrystone <barrystone3183@gmail.com>
2022-02-23 20:31:45 +0800

    [update] learning #each and insert a item in array by svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,31 @@
 	let firstName = 'Barry';
 	let lastName = 'Stone';
 	$: name = firstName + lastName;
-
 	let color = 'green';
-
 	const toggle = ()=>{
 		color = color==='green'?'red':'green';
 	}
-
 	let btn1Text = 'Continue';
-
 	let showText = false;
+
+	let users = [
+		{
+			id:'1',
+			name:'Barry'
+		},
+		{
+			id:'2',
+			name:'Jack'
+		},
+		{
+			id:'3',
+			name:'Tom'
+		},
+		
+	];
+	let usernameInput = '';
+	let idCount = 3;
+	let warning = '';
 
 </script>
 
@@ -28,14 +43,41 @@
 			I will become one of best developer.
 		</h2>
  	{/if}
-
 	<button on:click={()=>{ 
 		showText= !showText;
 		btn1Text= btn1Text==='Continue'?'Back':'Continue';
 	}}>{btn1Text}</button>
-	
 	<br/>
 	<button on:click={toggle}>change color to {color==='green'?'red':'green'}</button>
+	<br/>
+
+	<button on:click={()=>{
+		users = [...users,
+		{
+			id: idCount+1,
+			name: usernameInput
+		}]
+		warning= "This 'Add a user' button just one time available."
+				+ "</br>"
+				+ "If click it more than one time, the app just broke. only way to fix it is refresh page."
+				+ "</br>"
+				+ "Honest it, in this moment ,for a beginner, i don't know why...";
+
+	}}>Add a user
+	</button>
+	<input type="text" on:change={(e)=>usernameInput=e.target.value}>
+	<p>{@html warning}</p>
+
+	<ul style="list-style: none;">
+		{#each users as user (user.id)}
+				<li>{user.id}: {user.name}</li>
+		{/each}
+	</ul>
+	
+	
+
+
+
 
 </main>
 
