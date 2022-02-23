bf77ddd37576cdb514987265db6cfbb990bd9259
barrystone <barrystone3183@gmail.com>
2022-02-23 20:43:29 +0800

    [update] setup for feedback project

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,21 @@
 <script>
+	let feedbacks = [
+		{
+			id:1,
+			rating:8,
+			text:'good! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies integer quis auctor elit sed vulputate mi sit amet. Fringilla phasellus faucibus scelerisque eleifend donec.'
+		},
+		{
+			id:1,
+			rating:3,
+			text:'bad!!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies integer quis auctor elit sed vulputate mi sit amet. Fringilla phasellus faucibus scelerisque eleifend donec.'
+		},
+		{
+			id:1,
+			rating:10,
+			text:'great!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies integer quis auctor elit sed vulputate mi sit amet. Fringilla phasellus faucibus scelerisque eleifend donec.'
+		}
+	]
 </script>
 
 <main>
