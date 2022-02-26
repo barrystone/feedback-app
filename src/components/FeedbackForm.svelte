7b0462a84cafe8c4f7ee824cde2643040dcae9cc
barrystone <barrystone3183@gmail.com>
2022-02-26 17:13:48 +0800

    [feat] Buttron component, form bind: value to text in form and style

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,14 +1,57 @@
 <script>
+  import Button from './Button.svelte';
+  import Card from './Card.svelte';
+
+  export let text = '';
+  export let btnDisable = true;
 </script>
 
-<!-- Rating select form -->
-<form>
-  <div class="input-group">
-    <input
-      type="text"
-      placeholder="Tell us something that keeps you coming back
-    "
-    />
-    <button>Send</button>
-  </div>
-</form>
+<Card>
+  <header>
+    <h2>How would you rate your service with us ?</h2>
+  </header>
+  <!-- Rating select form -->
+  <form>
+    <div class="input-group">
+      <input
+        type="text"
+        bind:value={text}
+        placeholder="Tell us something that keeps you coming back"
+      />
+      <Button disabled={btnDisable} type="submit">Send</Button>
+    </div>
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+  input:focus {
+    outline: none;
+  }
+  .message {
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
